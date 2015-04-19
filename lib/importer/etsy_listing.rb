d73475da62a0c37b5cdc073db2539319c81e52e6
theMark <wingman42@gmail.com>
2015-04-19 00:04:04 -0700

    enter listing id in rake task to create product

--- lib/importer/etsy_listing.rb
+++ lib/importer/etsy_listing.rb
@@ -12,10 +12,52 @@ module Importer
     end
 
     # class method so use self.something
-    def self.get_listing
+    def self.get_listing(etsy_listing_id)
+      #connect to API
       Importer::EtsyListing.connect
-      listing = Etsy::Listing.find(157583414)
-      puts listing.inspect
+
+      #go find this listing
+      id = etsy_listing_id
+      l = Etsy::Listing.find(id)
+
+      title = l.title
+      description = l.description
+      etsy_listing_id = l.id
+      main_image_url = l.images.first.full || cheese
+      price = l.price
+      etsy_user_id = l.user_id
+
+      #FOR TESTING
+      # puts title
+      # puts description
+      # puts etsy_listing_id
+      # puts main_image_url
+      # puts price
+      # puts etsy_user_id
+
+
+      # Create new listing if it doesn't exist
+      if Listing.where(etsy_listing_id: etsy_listing_id).exists?
+        puts "That listing already exists for: #{etsy_listing_id}"
+      else
+        @listing = Listing.create(
+          title: title,
+          description: description,
+          etsy_listing_id: etsy_listing_id,
+          main_image_url: main_image_url,
+          price: price,
+          etsy_user_id: etsy_user_id
+          )
+        puts "new listing created: #{@listing.title}"
+      end
+
+
+
+
+
+
+
+
 
 
     end
--- lib/tasks/importer.rake
+++ lib/tasks/importer.rake
@@ -1,7 +1,14 @@
 namespace :importer do
   desc "Get listing"
-  task get_listing: :environment do
-    Importer::EtsyListing.get_listing
+  task :get_listing, [:id] => :environment do |task, args|
+    Importer::EtsyListing.get_listing(args.id)
   end
+
+
+# desc "Get listing"
+# task get_listing: :environment do
+#   Importer::EtsyListing.get_listing
+# end
+
 
 end
