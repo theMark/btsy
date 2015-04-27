bc8115b785845a07a1898855d6d221500950de4a
theMark <wingman42@gmail.com>
2015-04-27 01:23:05 -0700

    bunch of ui changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def etsy_listing_url(id)
+    url = "http://www.etsy.com/listing/#{id}"
+    return url
+  end
+
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,8 @@
 class Listing < ActiveRecord::Base
+
+
+def css_image_url
+  bg_url = main_image_url.sub(/^https?\:/, '')
+end
+
 end
